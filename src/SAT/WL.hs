f3027c2e87245f0cbb6f27ca854a151ee2ed8d6b
Jacob Edwards <jacob.john1@talktalk.net>
2025-01-25 14:37:47 +0000

    changes

--- src/SAT/WL.hs
+++ src/SAT/WL.hs
@@ -13,46 +13,38 @@ where
 import Data.Foldable (foldl')
 import Data.IntMap.Strict qualified as IntMap
 import Data.IntMap.Strict (type IntMap)
-import Data.List (nub)
-import Data.Sequence (type Seq)
-import Data.Sequence qualified as Seq
 import SAT.CNF (varOfLiteral, type Clause (Clause, literals, watched), type Literal)
 import SAT.Monad (type WatchedLiterals (WatchedLiterals), type ClauseDB)
 import Data.IntSet (type IntSet)
 import Data.IntSet qualified as IntSet
-import Utils (unstableIntNub)
-import Data.HashMap.Strict (type HashMap)
-import Data.HashSet (type HashSet)
-import Data.HashMap.Strict qualified as HashMap
-import Data.HashSet qualified as HashSet
+import Data.Sequence qualified as Seq
 
 initWatchedLiterals :: ClauseDB -> WatchedLiterals
 initWatchedLiterals clauseDB = WatchedLiterals litMap
   where
-    accumulate :: (Int, IntMap IntSet) -> Clause -> (Int, IntMap IntSet)
-    accumulate (cnt, !acc) (Clause {watched = (a, b), literals}) =
+    accumulate :: IntMap IntSet -> (Int, Clause) -> IntMap IntSet
+    accumulate acc (idx, Clause {watched = (a, b), literals}) =
       if a == b
         then 
-          (cnt + 1, IntMap.insertWith (<>) (varOfLiteral $ literals !! a) (IntSet.singleton cnt) acc)
-          -- (cnt + 1, acc)
+          IntMap.insertWith (<>) (varOfLiteral $ literals !! a) (IntSet.singleton idx) acc
         else
-          (cnt + 1, IntMap.insertWith (<>) (varOfLiteral $ literals !! a) (IntSet.singleton cnt) $
-            IntMap.insertWith (<>) (varOfLiteral $ literals !! b) (IntSet.singleton cnt) acc)
+          IntMap.insertWith (<>) (varOfLiteral $ literals !! a) (IntSet.singleton idx) $
+            IntMap.insertWith (<>) (varOfLiteral $ literals !! b) (IntSet.singleton idx) acc
 
     litMap :: IntMap.IntMap IntSet
-    litMap = snd $ foldl' accumulate (0, mempty) clauseDB
+    litMap = foldl' accumulate mempty (Seq.zip (Seq.fromList [0..length clauseDB]) clauseDB)
 
 initClauseWatched :: Clause -> Clause
-initClauseWatched (Clause {literals}) = Clause {literals = literals', watched = (a, b)}
+initClauseWatched (Clause {literals}) = Clause {literals = literals, watched = (a, b)}
   where
-    literals' = literals --unstableIntNub literals
 
     a :: Int
     b :: Int
-    (a, b) = findInitialWatched literals'
+    (a, b) = findInitialWatched literals
 
     findInitialWatched :: [Literal] -> (Int, Int)
-    findInitialWatched lits
-      | length lits == 1 = (0, 0)
-      | length lits > 1 = (0, 1)
-      | otherwise = error "Empty clause"
+    findInitialWatched lits = do 
+      let len = length lits
+      if len == 1
+        then (0, 0)
+        else (0, 1)
